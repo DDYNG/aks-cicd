7a97d223177f9f194e226906f7962ab803ae40e8
DDYNG <156653866+DDYNG@users.noreply.github.com>
2024-08-02 15:44:22 +0900

    Update App.vue

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <div>No name</div>
+    <div>2nd Edit</div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
